b6cd86077acf4ac0f02536c7f29d31ba205c37aa
gourav <gourav96suri@gmail.com>
2016-10-20 16:55:38 +0530

    Added edit/update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,31 @@ class ArticlesController < ApplicationController
 	def new 
 	end
 
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
 	def index
     	@articles = Article.all
   	end
 
 	def create
 		@article = Article.new(article_params)
- 		@article.save
-  		redirect_to @article
+ 		if @article.save
+    		redirect_to @article
+  		else
+    		render 'new'
+ 		 end
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+ 
+  		if @article.update(article_params)
+    		redirect_to @article
+ 		 else
+    		render 'edit'
+ 		 end
 	end
 
 	def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ActiveRecord::Base
+	validates :title, presence: true, length: {minimum: 5}
 end
