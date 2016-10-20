acc949af2a7cff37a028a370db240ef3d679d6de
gourav <gourav96suri@gmail.com>
2016-10-21 02:29:20 +0530

    Implemented all features of Blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,41 +1,53 @@
 class ArticlesController < ApplicationController
-	def new 
-	end
-
-	def edit
-  		@article = Article.find(params[:id])
-	end
-
+	
 	def index
-    	@articles = Article.all
-  	end
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
 
 	def create
-		@article = Article.new(article_params)
- 		if @article.save
-    		redirect_to @article
-  		else
-    		render 'new'
- 		 end
-	end
+		@article = Article.new(articles_params)
 
-	def update
-  		@article = Article.find(params[:id])
- 
-  		if @article.update(article_params)
-    		redirect_to @article
- 		 else
-    		render 'edit'
- 		 end
+		if @article.save
+			redirect_to articles_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
-    	@article = Article.find(params[:id])
-  	end
- 
-private
-  	def article_params
-    	params.require(:article).permit(:title, :text)
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(articles_params)
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article  =Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+
+	end
+
+	private
+
+	def articles_params
+		params.require(:article).permit(:title,:text)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019224306) do
+ActiveRecord::Schema.define(version: 20161020193842) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
